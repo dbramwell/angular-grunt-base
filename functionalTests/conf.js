c24dd6556a7469ef2517367f6e58851231f3e9b4
bram <david.j.bramwell@googlemail.com>
2017-01-21 12:15:51 -0200

    Moved e2e tests outside of client dir, added screenshot reporter

---
+++ functionalTests/conf.js
@@ -0,0 +1,43 @@
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var reporter = new HtmlScreenshotReporter({
+  dest: 'build/test/screenshots',
+  filename: 'functionalTestResults.html'
+});
+
+exports.config = {
+  directConnect: true,
+  capabilities: {
+    'browserName': 'chrome',
+    'chromeOptions': {
+      'args': ['no-sandbox']
+    }
+  },
+  framework: 'jasmine',
+  specs: ['**/*spec.js'],
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  },
+  baseUrl: 'http://localhost:3000',
+
+  // ...
+
+  // Setup the report before any tests start
+  beforeLaunch: function() {
+    return new Promise(function(resolve){
+      reporter.beforeLaunch(resolve);
+    });
+  },
+
+  // Assign the test reporter to each running instance
+  onPrepare: function() {
+    jasmine.getEnv().addReporter(reporter);
+  },
+
+  // Close the report after all tests finish
+  afterLaunch: function(exitCode) {
+    return new Promise(function(resolve){
+      reporter.afterLaunch(resolve.bind(this, exitCode));
+    });
+  }
+};
---
+++ functionalTests/navBar/test_spec.js
@@ -0,0 +1,31 @@
+describe('Nav Bar test', function() {
+
+	beforeEach(function() {
+		browser.get('/');
+	});
+
+	it('should have "David Bramwell" as header', function() {
+		expect(element(by.css('h1')).getText()).toEqual("David\nBramwell");
+	});
+
+	it('education button should have active class once clicked', function() {
+		expect(element(by.css('h1')).getText()).toEqual("David\nBramwell");
+		element(by.css('a[ui-sref="education"]')).click();
+		browser.wait(function() { return element(by.css('li.active')).isPresent() });
+		expect(element(by.css('li.active')).getText()).toEqual('Education');
+	});
+
+	it('Expertise button should have active class once clicked', function() {
+		expect(element(by.css('h1')).getText()).toEqual("David\nBramwell");
+		element(by.css('a[ui-sref="expertise"]')).click();
+		browser.wait(function() { return element(by.css('li.active')).isPresent() });
+		expect(element(by.css('li.active')).getText()).toEqual('Expertise');
+	});
+
+	it('Contact Me button should have active class once clicked', function() {
+		expect(element(by.css('h1')).getText()).toEqual("David\nBramwell");
+		element(by.css('a[ui-sref="contactMe"]')).click();
+		browser.wait(function() { return element(by.css('li.active')).isPresent() });
+		expect(element(by.css('li.active')).getText()).toEqual('Contact Me');
+	});
+});
